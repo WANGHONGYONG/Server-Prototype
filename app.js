ff339dcc23620c347ada10f5e1e5fd6125b0e2e4
kumi1688 <kumi1688@ajou.ac.kr>
2019-09-22 18:10:46 +0900

    set fundamental configuration

---
+++ app.js
@@ -0,0 +1,57 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var config = require('./config/config');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+console.log('view 엔진 ejs로 설정');
+
+console.log('config.server_port : %d', config.server_port);
+app.set('port', process.env.PORT || config.server_port);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var router = express.Router();
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+/*
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log('서버가 시작되었습니다. 포트 : ' + app.get('port'));
+
+  // 데이터베이스 초기화
+  database.init(app, config);
+
+});*/
+
+module.exports = app;
---
+++ config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    server_port: 3001,
+    db_url: 'mongodb://localhost:27017/local',
+}
